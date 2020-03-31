3b16d5466d3ebefc119f89b866c7f027f11ffd68
Matt Henry <matt.henry@owensboro.kyschools.us>
2020-03-31 10:24:17 -0500

    Refactored Endpoints to use a base class

--- Modules/3CX/Endpoints/ExtensionList.psm1
+++ Modules/3CX/Endpoints/ExtensionList.psm1
@@ -1,15 +1,20 @@
-Class ExtensionList
-{
-    $APIConnection
+Using module .\Endpoint.psm1
 
-    ExtensionList($APIConnection)
+Class ExtensionList : Endpoint
+{
+    
+    ExtensionList($APIConnection) : base($APIConnection)
     {
-        $this.APIConnection = $APIConnection
     }
 
     [Microsoft.PowerShell.Commands.WebResponseObject] New()
     {
         return $this.APIConnection.post('ExtensionList/new')
+    }
+
+    [Microsoft.PowerShell.Commands.WebResponseObject] Set()
+    {
+        return $this.APIConnection.post('ExtensionList/set')
     }
 
     [Microsoft.PowerShell.Commands.WebResponseObject] Update($payload)
