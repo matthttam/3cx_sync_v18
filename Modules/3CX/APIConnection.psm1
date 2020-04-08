505a38464b5e8fa4fd59695445c9b3c544e7e23c
Matt Henry <matt.henry@owensboro.kyschools.us>
2020-04-08 12:53:15 -0500

    Modified the setup program to store passwords in an array of hashtables so that multiple users can run the application. Modified webrequests to UseBasicParsing for when IE isn't configured for a user or isn't installed.

--- Modules/3CX/APIConnection.psm1
+++ Modules/3CX/APIConnection.psm1
@@ -12,7 +12,7 @@ class APIConnection
     APIConnection( [Config]$config )
     {
         $this.BaseUrl = $config.Config.BaseUrl.Trim('/')
-        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( ($config.Config.Password | ConvertTo-SecureString))
+        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( ( ($config.Config.Password | Where-Object -Property 'Username' -EQ ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name) | Select-Object -ExpandProperty 'Password') | ConvertTo-SecureString))
         $PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($BSTR)
         $this.Credentials = ((@{
             Username = $config.Config.Username
@@ -24,7 +24,7 @@ class APIConnection
 
     Login()
     {
-        $LoginResponse = Invoke-WebRequest -Uri ('{0}/login' -f $this.BaseUrl) -Method Post -ContentType "application/json;charset=UTF-8" -Body $this.Credentials.ToString() -SessionVariable 'Session'
+        $LoginResponse = Invoke-WebRequest -Uri ('{0}/login' -f $this.BaseUrl) -Method Post -ContentType "application/json;charset=UTF-8" -Body $this.Credentials.ToString() -SessionVariable 'Session' -UseBasicParsing
         $this.Session = Get-Variable -name Session -ValueOnly
         if( $LoginResponse.Content -ne 'AuthSuccess' ){
             Write-Error 'Failed to authenticate' -ErrorAction Stop
@@ -42,7 +42,7 @@ class APIConnection
     [PSObject] get([string]$Path)
     {
         $parameters = $this.ConnectionSettings
-        return (Invoke-WebRequest -Uri ('{0}/{1}' -f $this.BaseUrl, $Path) -Method Get @parameters)
+        return (Invoke-WebRequest -Uri ('{0}/{1}' -f $this.BaseUrl, $Path) -Method Get @parameters -UseBasicParsing)
     }
 
     [PSObject] post([string]$Path)
@@ -52,7 +52,7 @@ class APIConnection
     [PSObject] post([string]$Path, [hashtable] $Options)
     {
         $parameters = $this.ConnectionSettings
-        return Invoke-WebRequest -Uri ('{0}/{1}' -f $this.BaseUrl, $Path) -Method Post @parameters @Options
+        return Invoke-WebRequest -Uri ('{0}/{1}' -f $this.BaseUrl, $Path) -Method Post @parameters @Options -UseBasicParsing
     }
 
 }
--- Setup.ps1
+++ Setup.ps1
@@ -1,12 +1,36 @@
-﻿# Change directory
+﻿param(
+	[switch] $Update
+)
+function Write-Config(){
+    param(
+        [string] $ConfigPath,
+        [PSCustomObject] $Config
+    )
+    $config | ConvertTo-Json | Out-File -FilePath $ConfigPath -Confirm:$false
+}
+
+function Prompt-User(){
+    param(
+        [string] $Message,
+        $CurrentValue = "",
+        [switch] $Secure = $false
+    )
+
+    if($CurrentValue){
+        $Message += ' [{0}]' -f $CurrentValue
+    }
+    return Read-Host $Message -AsSecureString:$Secure
+}
+
+# Change directory
  $scriptpath = $MyInvocation.MyCommand.Path
  $dir = Split-Path $scriptpath
  Set-Location $dir
 
 $ConfigPath = Join-Path -Path $dir -ChildPath 'Config' | Join-Path -ChildPath 'config.json'
-
+$ConfigExists = (Test-Path -Path $ConfigPath)
 # If config file exists prompt if we should overwrite or quit
-if((Test-Path -Path $ConfigPath)){
+if( $ConfigExists -and -not $Update){
     $overwrite = Read-Host 'Config File Exists. Overwrite? [Yn]'
     if( -not ($overwrite -eq '' -or $overwrite.ToLower() -eq 'y') ){
         Write-Host 'Quitting'
@@ -14,19 +38,49 @@ if((Test-Path -Path $ConfigPath)){
     }
 }
 
-# Prompt for Configuration Information
-$username = Read-Host 'Enter the 3CX Admin Username'
-$password = Read-Host 'Enter the 3CX Admin Password' -AsSecureString
-$url = Read-Host 'Enter the 3CX api URL (e.g. https://site.my3cx.us:5001/api)'
-$importFilename = Read-Host 'Enter the import filename (e.g. import.csv)'
+if($ConfigExists -and $Update){
+    try{
+	    $CurrentConfig = Get-Content $ConfigPath | ConvertFrom-Json
+        
+    }catch{
+        Write-Error "Failed to read existing config file. Please run without the update flag to overwrite or create new." -ErrorAction Stop
+    }
+}
 
-$config = @{
-    Username = $username
-    Password = ConvertFrom-SecureString -SecureString $password
-    BaseUrl = $url.Trim('/')
-    ImportFilename = $importFilename
+if($Update){
+    Write-Host "Leave responses blank to keep the current value!"
+}
+# Prompt for Configuration Information
+$username = Prompt-User -Message 'Enter the 3CX Admin Username' -CurrentValue $CurrentConfig.Username
+$password = Prompt-User 'Enter the 3CX Admin Password' -Secure
+$url = Prompt-User 'Enter the 3CX api URL (e.g. https://site.my3cx.us:5001/api)' -CurrentValue $CurrentConfig.BaseUrl
+$importFilename = Prompt-User 'Enter the import filename (e.g. import.csv)' -CurrentValue $CurrentConfig.ImportFilename
+# -AsSecureString
+
+if($Update){
+    $config = $CurrentConfig
+}else{
+    $config = @{
+        Username = ""
+        Password = @{}
+        BaseUrl = ""
+        ImportFilename = ""
+    }
+}
+
+if($username){
+    $config.Username = $username
+}
+if($password.Length -ne 0){
+    #$config.Password += @{([System.Security.Principal.WindowsIdentity]::GetCurrent().Name) = (ConvertFrom-SecureString -SecureString $password)}
+    $config.Password += @{'Username' = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name; 'Password' = (ConvertFrom-SecureString -SecureString $password)}
+}
+if($url){
+    $config.BaseUrl = $url.Trim('/')
+}
+if($importFilename){
+    $config.ImportFilename = $importFilename
 }
 
 # Save config.json with encrypted password and other config information
-$config | ConvertTo-Json | Out-File -FilePath $ConfigPath -Confirm:$false
-
+Write-Config -ConfigPath $ConfigPath -Config $config
