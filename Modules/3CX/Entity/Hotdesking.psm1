a0e17830ee1e06d730a7c9ea695c7a36dd942c07
Matt Henry <matt.henry@owensboro.kyschools.us>
2020-07-17 15:53:36 -0500

    Added properties that are easily accessible and needed for new entity creation in 3CX. Added GetName function to return the Hotdesk HD0000 number.

--- Modules/3CX/Entity/Hotdesking.psm1
+++ Modules/3CX/Entity/Hotdesking.psm1
@@ -5,9 +5,16 @@ Class Hotdesking : Entity
 {
     [HotdeskingListEndpoint] $_endpoint
     [Hashtable] $PossibleValueLookup = @{}
-
+    [String] $MacAddress
+    [String] $Model
     Hotdesking($object, $endpoint) : base($object, $endpoint)
     {
+        $this.MacAddress = $object.ActiveObject.PhoneDevices._value.MacAddress._value;
+        $this.Model = $object.ActiveObject.PhoneDevices._value.Model._value;
+    }
+
+    [string] GetName(){
+        return $this.object.FirstName._value
     }
 
 }
