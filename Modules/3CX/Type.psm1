0fdebc2014fd0dfd36f23281073638f72e72491b
Matt Henry <matt.henry@owensboro.kyschools.us>
2020-08-31 14:14:19 -0500

    Create a type class specific to 3CX to allow easy referencing of Type to type ID.

---
+++ Modules/3CX/Type.psm1
@@ -0,0 +1,16 @@
+class Type {
+    static [string] $String = 3
+    static [string] $Enum = 11
+    static [string] $SelectedItem = 10
+    static [string] $Boolean = 0
+    static [string] $Collection = 12
+    static [string] $File = 14
+    static [string] $Item = 9 #13?
+    static [string] $ItemSet = 13 #9?
+    static [string] $TimeRanges = 6
+
+    Type([PSCustomObject] $config)
+    {
+        
+    }
+}
