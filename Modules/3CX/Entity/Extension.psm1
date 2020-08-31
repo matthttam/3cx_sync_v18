02d434bd5d3a3a9de37c9337a3e8a21d05332e64
Matt Henry <matt.henry@owensboro.kyschools.us>
2020-08-31 14:18:43 -0500

    No longer require storing as its own property. This can easily be accessed from the object attribute in Entity.

--- Modules/3CX/Entity/Extension.psm1
+++ Modules/3CX/Entity/Extension.psm1
@@ -4,9 +4,11 @@ Using module ..\Endpoints\ExtensionListEndpoint.psm1
 Class Extension : Entity
 {
     [ExtensionListEndpoint] $_endpoint
+   # [String] $number
 
     Extension($object, $endpoint) : base($object, $endpoint)
     {
+        #$this.number = $object.number
     }
     
 }
