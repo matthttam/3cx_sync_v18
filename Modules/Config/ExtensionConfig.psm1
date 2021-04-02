881552f16bc8933fb5d4d529b372f909e07d62f7
Matt Henry <matt.henry@owensboro.kyschools.us>
2021-04-02 16:13:35 -0500

    Fix number mapping for new extensions

--- Modules/Config/ExtensionConfig.psm1
+++ Modules/Config/ExtensionConfig.psm1
@@ -11,6 +11,9 @@ class ExtensionConfig : Config
     [array] $RequiredFields = @('key', 'threshold')
     
     ExtensionConfig( [string] $FullPath ) : base($FullPath){
+        # Set Key
+        $this.SetKey($this.Config.Key)
+
         # Store Threshold Values
         if($this.config.Threshold.Add){
             $this.SetThreshold("Add", $this.Config.Threshold.Add)
@@ -19,12 +22,14 @@ class ExtensionConfig : Config
             $this.SetThreshold("Disable", $this.Config.Threshold.Disable)
         }
 
+        # Set Number and key in the New Mapping config if not set
+        if( -not $this.Config.New['Number']){
+            $this.Config.New['Number'] = $this.GetKey()
+        }
+
         # Create Mappings for New and Update
         $this.Mapping.New = [ExtensionMapping]::new($this.Config.New)
         $this.Mapping.Update = [ExtensionMapping]::new($this.Config.Update)
-
-        # Set Key
-        $this.SetKey($this.Config.Key)
     }
 
     # Sets/Gets Key Settings
